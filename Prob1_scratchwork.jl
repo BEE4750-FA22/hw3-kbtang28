8e880baf15121cbc1041e76ef24981f07efca38d
Katerina Tang <kbt28@cornell.edu>
2022-10-17 16:01:10 -0400

    Generate requested plots

--- Prob1_scratchwork.jl
+++ Prob1_scratchwork.jl
@@ -1,6 +1,7 @@
 using JuMP
 using HiGHS
 using DataFrames
+using Plots
 
 # generator data
 investment_cost = [457000, 268000, 85000, 62580, 92000, 92000]
@@ -20,7 +21,7 @@ T = 1:length(hours)
 
 @variable(gencap, x[G] >= 0)
 @variable(gencap, y[G, T] >= 0)
-@objective(gencap, Min, investment_cost'*x + sum(op_cost .* [sum(y[g, :]) for g in G]) + 9000*(sum(demand)-sum(y)))
+@objective(gencap, Min, (investment_cost'*x) + 365*sum(op_cost .* [sum(y[g, :]) for g in G]) + 1000*365*(sum(demand)-sum(y)))
 
 avail = vcat(repeat(thermal_cf, 1, 24), wind_cf', solar_cf')
 @constraint(gencap, availability[g in G, t in T], y[g, t] <= avail[g, t]*x[g])
@@ -28,5 +29,15 @@ avail = vcat(repeat(thermal_cf, 1, 24), wind_cf', solar_cf')
 @constraint(gencap, load[t in T], sum(y[:, t]) <= demand[t])
 
 optimize!(gencap)
-objective_value(gencap)
+# objective_value(gencap) # total cost (over one year)
 
+# value.(x) # how much capacity should be installed for each generator type
+
+# unmet_demand = demand .- [sum(value.(y).data[:, t] for t in T)] # non-served demand
+
+# plot generated electricity by generator type over one day
+# plot(value.(y).data', xlabel="Time (hr)", ylabel="Generated electricity (MW)", label=["Geothermal" "Coal" "CCGT" "CT" "Wind" "Solar"], linewidth=2)
+
+# stacked area plot
+# areaplot(value.(y).data', xlabel="Time (hr)", ylabel="Generated electricity (MW)", label=["Geothermal" "Coal" "CCGT" "CT" "Wind" "Solar"])
+# plot!(demand, linewidth=2, linecolor=:black, label="Demand")
